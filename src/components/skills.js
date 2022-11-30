022a6af6a0e32822d77559c55097f181dcb88c6a
speaktosameer <speaktosameernp@gmail.com>
2022-11-30 20:42:18 +0545

    skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,5 +1,10 @@
+import { Container, Row, Col } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import 'react-multi-carousel/lib/styles.css';
+import meter1 from '../assets/img/meter1.svg';
+import meter2 from '../assets/img/meter2.svg';
+import meter3 from '../assets/img/meter3.svg';
+import colorSharp from '../assets/img/color-sharp.png';
 
 export const Skills = () => {
     const responsive ={
@@ -24,5 +29,39 @@ export const Skills = () => {
             items:1
         },
     };
+
+    return(
+        <section className="skill" id="skills">
+            <Container>
+                <Row>
+                    <Col>
+                    <div className="skill-bx">
+                        <h2>Skills</h2>
+                        <p>LoremThe href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, b</p>
+                        <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                            <div className="item">
+                                <img src={meter1} alt="Image"/>
+                                <h5>Web Developemnt</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter2} alt="Image"/>
+                                <h5>Brand Identity</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="Image"/>
+                                <h5>Web Design</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter1} alt="Image"/>
+                                <h5>Mobile Developement</h5>
+                            </div>
+                        </Carousel>
+                    </div>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-left" src= {colorSharp}/>
+        </section>
+    )
 
 }
